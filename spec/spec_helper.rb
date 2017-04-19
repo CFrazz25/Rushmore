4aa79e3107a4272af519dcd6d3595420c7faf75c
Chris Fraz & Derek Georgevich <pair+cfrazz25.dgeorgevich@devbootcamp.com>
2017-04-19 12:21:47 -0500

    Added to spec_helper to prevent'at_exit'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,4 +96,18 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+
+
+if defined?(RUBY_ENGINE) && RUBY_ENGINE == "ruby" && RUBY_VERSION >= "1.9"
+  module Kernel
+    alias :__at_exit :at_exit
+    def at_exit(&block)
+      __at_exit do
+        exit_status = $!.status if $!.is_a?(SystemExit)
+        block.call
+        exit exit_status if exit_status
+      end
+    end
+  end
+end
 end
