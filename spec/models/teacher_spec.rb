c9be501e89317f022a5233f2a12458198dc84ed3
Brendan White & Elsie Koglin <pair+brendanjwh.elsiekoglin@devbootcamp.com>
2017-04-20 11:15:07 -0500

    Working through Teacher testing

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Teacher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+describe "Teacher attributes" do
+  let(:teacher) { Teacher.new(name: "Mikey B", email: "mikeyb@gmail.com", password: "password") }
+
+    it "has a name" do
+      expect(teacher.name).to eq "Mikey B"
+    end
+
+    it "has an email address" do
+      ecpect(teacher.email).to
+    end
+
+  end
 end
