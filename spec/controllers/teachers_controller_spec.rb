1488f479242150e164f56837791140c2a39f65ec
Brendan White & Elsie Koglin <pair+brendanjwh.elsiekoglin@devbootcamp.com>
2017-04-20 16:03:19 -0500

    First teacher controller test green

--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -1,19 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Teacher, type: :controller do
-
-describe "Teacher attributes" do
-  let(:teacher) { Teacher.new(name: "Mikey B", email: "mikeyb@gmail.com", password: "password") }
-
-  # describe "POST #create" do
-  #   context "when valid params are passed" do
-  #     it "redirects to home page" do
-  #       post :create, { teacher: { name: "Test Person", email: "testperson@gmail.com", password: "password" } }
-  #       expect(response).to redirect_to students_path
-  #     end
-  #   end
-  # end
+RSpec.describe TeachersController, type: :controller do
+  # let(:teacher) { Teacher.new(name: "Mikey B", email: "mikeyb@gmail.com", password: "password") }
 
 
+
+  describe "POST #create" do
+    it "redirects to home page" do
+      teacher = Teacher.create(name: "Test Person", email: "testperson@gmail.com", password: "password")
+
+      post :create, {:teacher => {name: "Test Person", email: "testperson@gmail.com", password: "password"} }
+      expect(response).to have_http_status 200
+    end
   end
+
+
 end
